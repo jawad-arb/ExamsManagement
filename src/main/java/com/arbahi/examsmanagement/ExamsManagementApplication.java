13055fbb42b8f2c23734e37c52c7d4d5b2018f8b
ARBAHI Jawad <106961582+jawad-arb@users.noreply.github.com>
2024-04-25 23:56:12 +0100

    first commit

---
+++ src/main/java/com/arbahi/examsmanagement/ExamsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.arbahi.examsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamsManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arbahi/examsmanagement/ExamsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbahi.examsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamsManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
